c572bdd4b4c22ca3c2620a5554fac7dca3fd37a4
Samet <sametay153@gmail.com>
2018-06-05 02:04:49 +0300

    get message content-sender-args and help command with role control

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ parsbot.on('ready', () => { // ready control
 
 parsbot.on('message', message => { // get messages
 
+    let msg = message.content;
+    let msgUpper = message.content.toUpperCase();
+    let sender = message.author;
+    let args = msg.split(" ");
 
+    if (msgUpper === prefix + 'YARDIM') { // help command
+        if(!message.member.roles.find(r=>["Parsbot"].includes(r.name))) // role control
+        {
+            message.channel.send(sender + " botu kullanım yetkiniz yok")
+        }
+        else
+        {
+        var help_image = "https://i.imgsafe.org/5c/5c35c8adf3.png";
+        const embed = new Discord.RichEmbed() // define embed and send
+            .setThumbnail(help_image)
+            .setColor(1C8ADB)
+            .setAuthor('Komutlar:')
+            .addField('Yardım', 'Kullanımı: !yardım')
+            message.channel.send(embed);
+        }
+    }
 
 });
