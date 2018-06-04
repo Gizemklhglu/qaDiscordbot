6a40347ced59ecc7d8d478ee3d31e6bc5d72f976
Samet <sametay153@gmail.com>
2018-06-05 01:39:01 +0300

    const prefix / client, login and get messages

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); 
+const prefix = '!'; // message prefix
+const parsbot = new Discord.Client();
+
+parsbot.login(''); // discord token login
+
+parsbot.on('ready', () => { // ready control
+    console.log('Parsbot started')
+});
+
+parsbot.on('message', message => { // get messages
+
+
+
+});
